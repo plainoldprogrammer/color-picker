964400198ca700700808d422db22a754e64d228d
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-06-21 20:22:05 -0500

    renaming and configuring the main window

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -28,16 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // MainWindow
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "MainWindow";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
+			this.SuspendLayout();
+			// 
+			// MainWindow
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(347, 269);
+			this.Name = "MainWindow";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
 
         }
 
--- Program.cs
+++ Program.cs
@@ -5,16 +5,16 @@ using System.Windows.Forms;
 
 namespace color_picker
 {
-    static class Program
-    {
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            MainWindow mainWindow = new MainWindow();
-               mainWindow.Text = "Color Picker";
-            Application.Run(mainWindow);
-        }
-    }
+	static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			MainWindow mainWindow = new MainWindow();
+			mainWindow.Text = "Color Picker";
+			Application.Run(mainWindow);
+		}
+	}
 }
