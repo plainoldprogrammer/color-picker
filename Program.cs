db0d957d7eb50573b26110ce7ce8028ca7748698
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-06-21 20:14:22 -0500

    initializing the project in VS 2019 Community Edition

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace color_picker
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            MainWindow mainWindow = new MainWindow();
+               mainWindow.Text = "Color Picker";
+            Application.Run(mainWindow);
+        }
+    }
+}
