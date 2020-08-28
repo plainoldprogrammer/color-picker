6ed6be24b9a392c41b549b0ec947f8a16109cc45
plainoldprogrammer <plainoldprogrammer@gmail.com>
2020-08-27 22:00:33 -0500

    checking the valid range for the red color

--- MainWindow.cs
+++ MainWindow.cs
@@ -43,6 +43,8 @@ namespace color_picker
 
         private void textBoxRedValue_TextChanged(object sender, EventArgs e)
         {
+			int redValue = Int32.Parse(textBoxRedValue.Text);
+
 			if (textBoxRedValue.Text.Equals(""))
 			{
 				redValue = 0;
@@ -52,11 +54,13 @@ namespace color_picker
 				redValue = Int32.Parse(textBoxRedValue.Text);
 			}
 
-            System.Drawing.SolidBrush brush = new System.Drawing.SolidBrush(Color.FromArgb(redValue, 0, 0));
-            System.Drawing.Graphics panelGraphics = panelColorPicker.CreateGraphics();
-
-            panelGraphics.FillRectangle(brush, new Rectangle(0, 0, 180, 80));
-			changePanelColorResulting();
+			if (redValue >= 0 && redValue <= 255)
+			{
+				System.Drawing.SolidBrush brush = new System.Drawing.SolidBrush(Color.FromArgb(redValue, 0, 0));
+				System.Drawing.Graphics panelGraphics = panelColorPicker.CreateGraphics();
+				panelGraphics.FillRectangle(brush, new Rectangle(0, 0, 180, 80));
+				changePanelColorResulting();
+			}
         }
 
         private void textBoxGreenValue_TextChanged(object sender, EventArgs e)
