5c83cf864dbf14ef3afeab0b47d6143528540e35
plainoldprogrammer <plainoldprogrammer@gmail.com>
2020-08-27 22:09:11 -0500

    checking when the red color value is not empty

--- MainWindow.cs
+++ MainWindow.cs
@@ -43,23 +43,26 @@ namespace color_picker
 
         private void textBoxRedValue_TextChanged(object sender, EventArgs e)
         {
-			int redValue = Int32.Parse(textBoxRedValue.Text);
+			if (textBoxRedValue.Text.Length != 0)
+			{
+				int redValue = Int32.Parse(textBoxRedValue.Text);
 
-			if (textBoxRedValue.Text.Equals(""))
-			{
-				redValue = 0;
-			}
-			else
-			{
-				redValue = Int32.Parse(textBoxRedValue.Text);
-			}
+				if (textBoxRedValue.Text.Equals(""))
+				{
+					redValue = 0;
+				}
+				else
+				{
+					redValue = Int32.Parse(textBoxRedValue.Text);
+				}
 
-			if (redValue >= 0 && redValue <= 255)
-			{
-				System.Drawing.SolidBrush brush = new System.Drawing.SolidBrush(Color.FromArgb(redValue, 0, 0));
-				System.Drawing.Graphics panelGraphics = panelColorPicker.CreateGraphics();
-				panelGraphics.FillRectangle(brush, new Rectangle(0, 0, 180, 80));
-				changePanelColorResulting();
+				if (redValue >= 0 && redValue <= 255)
+				{
+					System.Drawing.SolidBrush brush = new System.Drawing.SolidBrush(Color.FromArgb(redValue, 0, 0));
+					System.Drawing.Graphics panelGraphics = panelColorPicker.CreateGraphics();
+					panelGraphics.FillRectangle(brush, new Rectangle(0, 0, 180, 80));
+					changePanelColorResulting();
+				}
 			}
         }
 
