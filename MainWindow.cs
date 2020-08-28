63a2a852ba72b836f41a1cf860e75d320a5337cf
plainoldprogrammer <plainoldprogrammer@gmail.com>
2020-08-27 22:10:50 -0500

    checking when the green color value is not empty

--- MainWindow.cs
+++ MainWindow.cs
@@ -68,23 +68,26 @@ namespace color_picker
 
         private void textBoxGreenValue_TextChanged(object sender, EventArgs e)
         {
-			int greenValue = Int32.Parse(textBoxGreenValue.Text);
+			if (textBoxGreenValue.Text.Length != 0)
+			{
+				int greenValue = Int32.Parse(textBoxGreenValue.Text);
 
-			if (textBoxGreenValue.Text.Equals(""))
-			{
-				greenValue = 0;
-			}
-			else
-			{
-				greenValue = Int32.Parse(textBoxGreenValue.Text);
-			}
+				if (textBoxGreenValue.Text.Equals(""))
+				{
+					greenValue = 0;
+				}
+				else
+				{
+					greenValue = Int32.Parse(textBoxGreenValue.Text);
+				}
 
-			if (greenValue >= 0 && greenValue <= 255)
-			{
-				System.Drawing.SolidBrush brush = new System.Drawing.SolidBrush(Color.FromArgb(0, greenValue, 0));
-				System.Drawing.Graphics panelGraphics = panelColorPicker.CreateGraphics();
-				panelGraphics.FillRectangle(brush, new Rectangle(0, 80, 180, 80));
-				changePanelColorResulting();
+				if (greenValue >= 0 && greenValue <= 255)
+				{
+					System.Drawing.SolidBrush brush = new System.Drawing.SolidBrush(Color.FromArgb(0, greenValue, 0));
+					System.Drawing.Graphics panelGraphics = panelColorPicker.CreateGraphics();
+					panelGraphics.FillRectangle(brush, new Rectangle(0, 80, 180, 80));
+					changePanelColorResulting();
+				}
 			}
         }
 
